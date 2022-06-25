e2feb0e13b973562631f3231e25ec4df75d19211
benyossef27 <93250111+benyossef27@users.noreply.github.com>
2022-06-25 10:55:51 +0300

    cors fix

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ const NotFoundError = require('./errors/not-found-err');
 const { PORT = 3000 } = process.env;
 
 const app = express();
-app.use(cors());
+app.use('*', cors());
 app.use(limiter);
 mongoose.connect('mongodb://localhost:27017/aroundb');
 app.use(express.json());
