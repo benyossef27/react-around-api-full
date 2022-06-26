7e978f549f6591f6834a9d8203250ac3b29dc664
benyossef27 <93250111+benyossef27@users.noreply.github.com>
2022-06-26 10:07:42 +0300

    validate login and singup doby rq

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const helmet = require('helmet');
-const { errors } = require('celebrate');
+const { errors, celebrate, Joi } = require('celebrate');
 const cors = require('cors');
 const userRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
@@ -23,8 +23,29 @@ app.use(helmet());
 
 app.use(requestLogger);
 app.use(limiter);
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post(
+  '/signin',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+    }),
+  }),
+  login
+);
+app.post(
+  '/signup',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30).pattern(new RegExp('^[a-zA-Z-\\s]*$')),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string().uri(),
+      email: Joi.string().required().email(),
+      password: Joi.string().min(8).alphanum().required(),
+    }),
+  }),
+  createUser
+);
 app.use(auth);
 app.use('/users', userRouter);
 app.use('/cards', cardsRouter);
