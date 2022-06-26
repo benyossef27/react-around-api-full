922b9bb4f753acd794fbbcc2b7f7edd121624a77
benyossef27 <93250111+benyossef27@users.noreply.github.com>
2022-06-26 09:56:09 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ app.options('*', cors());
 mongoose.connect('mongodb://localhost:27017/aroundb');
 app.use(express.json());
 app.use(helmet());
-app.use(errors());
+
 app.use(requestLogger);
 app.use(limiter);
 app.post('/signin', login);
@@ -35,7 +35,9 @@ app.get('*', (req, res) => {
 });
 app.put('/cards/:cardId/likes', cardsRouter);
 app.delete('/cards/:cardId/likes', cardsRouter);
+app.use(errors());
 app.use(errorLogger);
+
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
   res.status(statusCode).send({
