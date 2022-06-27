5dcf7933a896c1895278905be0b63dc38f276bdc
benyossef27 <93250111+benyossef27@users.noreply.github.com>
2022-06-27 11:59:57 +0300

    Update users.js

--- backend/controlers/users.js
+++ backend/controlers/users.js
@@ -79,7 +79,7 @@ module.exports.updateAvatar = (req, res, next) => {
 };
 module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
-  User.findUserByCredentials(req.body)
+  User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign(
         { _id: user._id },
