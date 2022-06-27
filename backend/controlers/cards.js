d5c03c44dd30136302986f4b1f2e4015d46bd72e
benyossef27 <93250111+benyossef27@users.noreply.github.com>
2022-06-27 09:27:33 +0300

    Update cards.js

--- backend/controlers/cards.js
+++ backend/controlers/cards.js
@@ -17,7 +17,7 @@ module.exports.deleteCard = (req, res, next) => {
 
 module.exports.getCards = (req, res, next) => {
   Card.find({})
-    .then((cards) => res.send(cards))
+    .then((cards) => res.send({ cards }))
     .catch((err) => next(err));
 };
 
