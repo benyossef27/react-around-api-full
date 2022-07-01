e4f25d8b3d77574ac0b680b8da931817036d0549
benyossef27 <93250111+benyossef27@users.noreply.github.com>
2022-07-01 17:43:16 +0300

    Update users.js

--- backend/controlers/users.js
+++ backend/controlers/users.js
@@ -30,8 +30,8 @@ module.exports.getUser = (req, res, next) => {
 module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
   User.findUserByCredentials({ email, password })
-    .select('+password')
     .orFail(() => new AuthError('Incorrect email or password.'))
+    .select('+password')
     .then((user) => {
       const token = jwt.sign(
         { _id: user._id },
