e299734ef761f573b0f1be6a45f216f5dc14fe78
benyossef27 <93250111+benyossef27@users.noreply.github.com>
2022-07-01 12:04:08 +0300

    Update users.js

--- backend/controlers/users.js
+++ backend/controlers/users.js
@@ -72,11 +72,11 @@ module.exports.createUser = (req, res, next) => {
         avatar,
         email,
         password,
+      }).catch(() => {
+        next(new ConflictError('User already exists'));
       })
     )
-    .catch(() => {
-      next(new ConflictError('User already exists'));
-    })
+
     .then((user) => {
       res.status(201).send({ id: user._id });
     })
